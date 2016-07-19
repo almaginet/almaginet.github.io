db7c50016772fce45190f05df455c963136e323f
Juan Osorio <Juan Osorio>
2016-07-19 04:56:26 -0500

    'MENU'

---
+++ static/js/main.js
@@ -0,0 +1,115 @@
+function tlf()
+    {
+        if(document.getElementById('tlf').className == 'hidden')
+            {
+            document.getElementById('tlf').className = 'show';
+            } else {
+                document.getElementById('tlf').className = 'hidden';
+                    }
+    } 
+
+function inv()
+    {
+        if(document.getElementById('inv').className == 'hidden')
+            {
+            document.getElementById('inv').className = 'show';
+            } else {
+                document.getElementById('inv').className = 'hidden';
+                    }
+    } 
+
+function right01()
+    {
+        if(document.getElementById('right01').className == 'right-frameClose')
+            {
+            document.getElementById('right01').className = 'right-frame';
+            document.getElementById('right00').className = 'right-frameClose';
+            document.getElementById('right02').className = 'right-frameClose';
+            document.getElementById('right03').className = 'right-frameClose';
+            document.getElementById('right04').className = 'right-frameClose';
+            document.getElementById('right05').className = 'right-frameClose';
+            } else {
+                document.getElementById('right01').className = 'right-frameClose';
+                document.getElementById('right00').className = 'right-frame';
+                    }
+    } 
+
+function right02()
+    {
+        if(document.getElementById('right02').className == 'right-frameClose')
+            {
+            document.getElementById('right02').className = 'right-frame';
+            document.getElementById('right00').className = 'right-frameClose';
+            document.getElementById('right01').className = 'right-frameClose';
+            document.getElementById('right03').className = 'right-frameClose';
+            document.getElementById('right04').className = 'right-frameClose';
+            document.getElementById('right05').className = 'right-frameClose';
+            } else {
+                document.getElementById('right02').className = 'right-frameClose';
+                document.getElementById('right00').className = 'right-frame';
+                    }
+    }
+
+function right03()
+    {
+        if(document.getElementById('right03').className == 'right-frameClose')
+            {
+            document.getElementById('right03').className = 'right-frame';
+            document.getElementById('right00').className = 'right-frameClose';
+            document.getElementById('right02').className = 'right-frameClose';
+            document.getElementById('right01').className = 'right-frameClose';
+            document.getElementById('right04').className = 'right-frameClose';
+            document.getElementById('right05').className = 'right-frameClose';
+            } else {
+                document.getElementById('right03').className = 'right-frameClose';
+                document.getElementById('right00').className = 'right-frame';
+                    }
+    }
+
+function right04()
+    {
+        if(document.getElementById('right04').className == 'right-frameClose')
+            {
+            document.getElementById('right04').className = 'right-frame';
+            document.getElementById('right00').className = 'right-frameClose';
+            document.getElementById('right02').className = 'right-frameClose';
+            document.getElementById('right03').className = 'right-frameClose';
+            document.getElementById('right01').className = 'right-frameClose';
+            document.getElementById('right05').className = 'right-frameClose';
+            } else {
+                document.getElementById('right04').className = 'right-frameClose';
+                document.getElementById('right00').className = 'right-frame';
+                    }
+    }
+
+function right05()
+    {
+        if(document.getElementById('right05').className == 'right-frameClose')
+            {
+            document.getElementById('right05').className = 'right-frame';
+            document.getElementById('right00').className = 'right-frameClose';
+            document.getElementById('right02').className = 'right-frameClose';
+            document.getElementById('right03').className = 'right-frameClose';
+            document.getElementById('right04').className = 'right-frameClose';
+            document.getElementById('right01').className = 'right-frameClose';
+            } else {
+                document.getElementById('right05').className = 'right-frameClose';
+                document.getElementById('right00').className = 'right-frame';
+                    }
+    }
+
+function right00()
+    {
+        if(document.getElementById('right00').className == 'right-frameClose')
+            {
+            document.getElementById('right00').className = 'right-frame';
+            document.getElementById('right05').className = 'right-frameClose';
+            document.getElementById('right02').className = 'right-frameClose';
+            document.getElementById('right03').className = 'right-frameClose';
+            document.getElementById('right04').className = 'right-frameClose';
+            document.getElementById('right01').className = 'right-frameClose';
+            } else {
+                document.getElementById('right00').className = 'right-frameClose';
+                document.getElementById('right01').className = 'right-frame';
+                    }
+    }
